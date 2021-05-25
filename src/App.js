4e9b3fff58c5bff0f2c4f9c02769408f60f69ccf
unknown <you@example.com>
2021-05-25 15:25:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import HomePage from "./page/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
