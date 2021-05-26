bd521bb2f7dc1396a8d6e0d4c03e7e04d98394b8
unknown <you@example.com>
2021-05-26 13:15:24 +0530

    adding header to home and shop page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import HomePage from "./page/homepage/homepage.component";
 import ShopPage from "./page/shopPage/shop.component";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
